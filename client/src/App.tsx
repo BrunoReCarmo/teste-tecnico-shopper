639eac5e03c5c3d22a4ff7227d89b3b8e455e10d
BrunoReCarmo <bruno.reis.carmo@gmail.com>
2024-11-23 00:44:21 -0300

    Tailwind and typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+import React from 'react';
+import Maps from './pages/Maps';
+
+const App  = () => {
+  return (
+    <div className='w-screen h-screen'>
+      <Maps />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Maps.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Maps = () => {
+
+    return (
+        <div>
+        test
+        </div>
+    )
+}
+
+export default Maps
